7085e7b944876a290d248fd1152e8983eeddbc2b
martinalbanese <albanesemartinax@gmail.com>
2025-04-14 10:47:03 +0200

    aggiunti commenti a Button.stories.ts

--- src/Button.stories.ts
+++ src/Button.stories.ts
@@ -2,17 +2,17 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
-    component: Button,
-    title: 'Component/Button',
-    tags: ['autodocs']
+    component: Button, // Componente da testare
+    title: 'Component/Button', // Titolo del componente
+    tags: ['autodocs'] // Tags per la documentazione
 }
-export default meta;
+export default meta; // Esporta il meta per essere utilizzato in altri file
 
-type Story = StoryObj<typeof Button>;
+type Story = StoryObj<typeof Button>; //Definisce il tipo di storie per il compoente Button
 
 export const Primary: Story = {
     args: {
-        label: 'Click me',
-        onClick: () => alert('Clicked!')
+        label: 'Click me', // Label del bottone
+        onClick: () => alert('Clicked!') // Funzione da eseguire al click
     }
 }
