80c3927f14d1e2e3164bda464be031ec78a8bf27
martinalbanese <albanesemartinax@gmail.com>
2025-04-14 14:35:39 +0200

    correzioni al componente Button e relativa storia

--- src/Button.stories.ts
+++ src/Button.stories.ts
@@ -10,36 +10,29 @@ export default meta; // Esporta il meta per essere utilizzato in altri file
 
 type Story = StoryObj<typeof Button>; //Definisce il tipo di storie per il compoente Button
 
-export const Default: Story = {
-    args: {
-        // Label del bottone
-        label: 'Cliccami',
-
-        // Funzione da eseguire al click
-        onClick: () => new Promise<void>(resolve => {
-            setTimeout(() => {
-                alert('Cliccato!');
-                resolve();
-            }, 5000);
-        })
-    }
-};
-
-/* Nuova storia per il bottone
-- Modificare il componente Button per supportare la disabilitazione
-- Aggiungere una proprietà opzionale per disabilitarlo
-- Se la proprietà è null (non obbligatoria) gestire all'onClick prima la disabilitazione 
-(quando non viene passata ha un comportamento di default)
-- Simulare l'operazione del click con un setTimeout di 5 secondi */
-export const AsyncClick: Story = {
-    args: {
-        label: 'Cliccami per vedere il delay',
-        onClick: () => new Promise<void>(resolve => {
+const onClick = async () => {
+    return new Promise<void>(
+        (resolve) => {
             setTimeout(() => {
                 alert('Operazione completata dopo 5 secondi!');
                 resolve();
             }, 5000);
-        })
+        }
+    )
+}
+
+export const Default: Story = {
+    args: {
+        label: 'Cliccami',
+        onClick: onClick
+    }
+};
+
+export const Disabled: Story = {
+    args: {
+        label: 'Cliccami',
+        onClick: onClick,
+        isDisabled: true
     }
 }
 
--- src/Button.tsx
+++ src/Button.tsx
@@ -2,25 +2,27 @@ import React, { useState } from "react";
 
 type ButtonProps = {
     label: string;
-    onClick: () => void;
+    onClick: () => Promise<void>;
+    isDisabled?: boolean;
 }
 
 /**
  * Un componente Button riutilizzabile che mostra un'etichetta e gestisce gli eventi di click
- * @param {string} label - Il testo da visualizzare sul bottone
- * @param {() => void} onClick - Funzione di callback da eseguire quando il bottone viene cliccato
+ * @param label - Il testo da visualizzare sul bottone
+ * @param onClick - Funzione di callback da eseguire quando il bottone viene cliccato
+ * @param disableOnClick - Se true, il bottone viene disabilitato dopo il click
  * @returns Un bottone con l'etichetta e il gestore di click
 */
-export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-    const [isDisabled, setIsDisabled] = useState(false);
+export const Button: React.FC<ButtonProps> = ({ label, onClick, isDisabled }) => {
+    const [disabled, setDisabled] = useState(isDisabled);
 
     const handleClick = async () => {
-        setIsDisabled(true);
+        setDisabled(true);
         await onClick();
-        setIsDisabled(false);
+        setDisabled(false);
     };
 
-    return <button onClick={handleClick} disabled={isDisabled}>{label}</button>
+    return <button onClick={handleClick} disabled={disabled}>{label}</button>
 }
 
 
